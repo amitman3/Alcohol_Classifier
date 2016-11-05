0788a1224944876d8f9155cee6689e22c677f963
amitman3 <darthvader.mantri@gmail.com>
2016-11-05 23:26:49 +0530

    Updated V1: Updated comments and cleaned up unwanted code
    Updated logic to print best model from the lot (based on accuracy) along with confusion matrix for the same.
    Default response variable set to "Dalc" - Daily alcohol consumption.
    The program can also be used for prediction on other response variables like Walc (Weekend alcohol consumption) by simply changing the "response" variable in the code

--- Code/Alcohol_Classifier.R
+++ Code/Alcohol_Classifier.R
@@ -17,7 +17,9 @@ sum(is.na(input))
 
 par(mfrow = c(2,2))
 # test <- input
+# Change type to factor for factor fields
 input[,c(7,8,13,14,24:29)] <- as.data.frame(sapply(X = input[,c(7,8,13,14,24:29)],function(z) as.factor(z)))
+
 for (att in colnames(input)){
   att_class <- paste(att,class(input[,att]),sep = "_")
   #   print(class(input[,att]))
@@ -38,13 +40,12 @@ testing <- input[-inTrain,]
 
 # Set response variable for model
 response <- "Dalc"
-train_control <- trainControl(method = "cv",number = 10,savePredictions = TRUE)
-# Set seed to control for resampling and generate reproducible results
-set.seed(seed = 123)
+
 # Create empty final accuracy list
 model_output <- list()
 accuracy_df <- data.frame(classifiers,0) 
 colnames(accuracy_df) <- c("model","accuracy")
+
 # Call model builder for each classifier
 # Add progress bar
 for (classifier in classifiers){
@@ -64,110 +65,15 @@ for (classifier in classifiers){
   print(paste0("Completed prediction using ",classifier))
 }
 model_output[["model_accuracy"]] <- accuracy_df
-best_model <- as.character(accuracy_df[accuracy_df$accuracy==max(accuracy_df$accuracy),"model"])
+# Display accuracies of model in decreasing order
+print(paste0("Accuracy Table: "))
+print.data.frame(accuracy_df[order(accuracy_df$accuracy,decreasing = TRUE),])
+# Select the best model of the lot in terms of accuracy.
+# If multiple models with same accuracy, select the first from the lot
+best_model <- as.character(accuracy_df[accuracy_df$accuracy==max(accuracy_df$accuracy),"model"][1])
 print(paste0("Highest accuracy achieved with: ",best_model))
 # Need to check confusion matrix display
 print(paste0("Confusion matrix for ",best_model,": ",model_output[[best_model]][2]))
-
-
-modelrf <- train(Dalc~.,data = training,trControl = train_control,method = "rf")
-# modellinsvm <- train(training$Dalc~.,data = training,trControl = train_control,method = "lssvmLinear")
-# modelpolysvm <- train(training$Dalc~.,data = training,trControl = train_control,method = "lssvmPoly")
-# modelradsvm <- train(training$Dalc~.,data = training,trControl = train_control,method = "lssvmRadial")
-modellinsvm <- train(training$Dalc~.,data = training,trControl = train_control,method = "svmLinear")
-modelpolysvm <- train(training$Dalc~.,data = training,trControl = train_control,method = "svmPoly")
-# modelradsvm <- train(training$Dalc~.,data = training,trControl = train_control,method = "svmRadial")
-modelnnet <- train(training$Dalc~.,data = training,trControl = train_control,method = "avNNet")
-modellda <- train(training$Dalc~.,data = training,trControl = train_control,method = "lda2")
-modelmnom <- train(training$Dalc~.,data = training,trControl = train_control,method = "multinom")
-# modelxgbLin <- train(training$Dalc~.,data = training,trControl = train_control,method = "xgbLinear")
-
-# Random forest stats
-rffit <- predict.train(modelrf,newdata = testing)
-rfprob <- predict.train(modelrf,newdata = testing,type = "prob")
-head(rffit)
-confusionMatrix(data = rffit,testing$Dalc)
-
-# Neural network stats
-nnetfit <- predict.train(modelnnet,newdata = testing)
-nnetprob <- predict.train(modelnnet,newdata = testing,type = "prob")
-head(nnetfit)
-confusionMatrix(data = nnetfit,testing$Dalc)
-
-# lda stats
-ldafit <- predict.train(modellda,newdata = testing)
-ldaprob <- predict.train(modellda,newdata = testing,type = "prob")
-head(ldafit)
-confusionMatrix(data = ldafit,testing$Dalc)
-
-# Multinom stats
-mnomfit <- predict.train(modelmnom,newdata = testing)
-mnomprob <- predict.train(modelmnom,newdata = testing,type = "prob")
-head(mnomfit)
-confusionMatrix(data = mnomfit,testing$Dalc)
-
-# Linear SVM stats
-lsvmfit <- predict.train(modellinsvm,newdata = testing)
-lsvmprob <- predict.train(modellinsvm,newdata = testing,type = "prob")
-head(lsvmfit)
-confusionMatrix(data = lsvmfit,testing$Dalc)
-
-# Polynomial SVM stats
-polysvmfit <- predict.train(modelpolysvm,newdata = testing)
-psvmprob <- predict.train(modelpolysvm,newdata = testing,type = "prob")
-head(polysvmfit)
-confusionMatrix(data = polysvmfit,testing$Dalc)
-
-## Radial SVM stats
-# radsvmfit <- predict.train(modelradsvm,newdata = testing)
-# rsvmprob <- predict.train(modelradsvm,newdata = testing,type = "prob")
-# head(radsvmfit)
-# confusionMatrix(data = radsvmfit,testing$Dalc)
-
-## Extreme Gradient Boosting stats
-# xgbfit <- predict.train(modelxgbLin,newdata = testing)
-# xgbprob <- predict.train(modelxgbLin,newdata = testing,type = "prob")
-# head(xgbfit)
-# confusionMatrix(data = xgbfit,testing$Dalc)
-
-# =============================================================================================
-# Classification based on Weekend alcohol consumption
-
-modelrf <- train(training$Walc~.,data = training,trControl = train_control,method = "rf")
-# modellinsvm <- train(training$Walc~.,data = training,trControl = train_control,method = "lssvmLinear")
-# modelpolysvm <- train(training$Walc~.,data = training,trControl = train_control,method = "lssvmPoly")
-# modelradsvm <- train(training$Walc~.,data = training,trControl = train_control,method = "lssvmRadial")
-modellinsvm <- train(training$Walc~.,data = training,trControl = train_control,method = "svmLinear")
-modelpolysvm <- train(training$Walc~.,data = training,trControl = train_control,method = "svmPoly")
-modelradsvm <- train(training$Walc~.,data = training,trControl = train_control,method = "svmRadial")
-modelnnet <- train(training$Walc~.,data = training,trControl = train_control,method = "avNNet")
-modellda <- train(training$Walc~.,data = training,trControl = train_control,method = "lda2")
-modelmnom <- train(training$Walc~.,data = training,trControl = train_control,method = "multinom")
-# modelxgbLin <- train(training$Walc~.,data = training,trControl = train_control,method = "xgbLinear")
-
-# Random forest stats
-rffit <- predict.train(modelrf,newdata = testing)
-confusionMatrix(data = rffit,testing$Walc)
-
-lsvmfit <- predict.train(modellinsvm,newdata = testing)
-confusionMatrix(data = lsvmfit,testing$Walc)
-
-psvmfit <- predict.train(modelpolysvm,newdata = testing)
-confusionMatrix(data = psvmfit,testing$Walc)
-
-rsvmfit <- predict.train(modelradsvm,newdata = testing)
-confusionMatrix(data = rsvmfit,testing$Walc)
-
-nnetfit <- predict.train(modelnnet,newdata = testing)
-confusionMatrix(data = nnetfit,testing$Walc)
-
-ldafit <- predict.train(modellda,newdata = testing)
-confusionMatrix(data = ldafit,testing$Walc)
-
-mnomfit <- predict.train(modelmnom,newdata = testing)
-confusionMatrix(data = mnomfit,testing$Walc)
-response <- "Dalc"
-classifier <- "rf"
 
 buildModel <- function(response,classifier){
   #   Setting train control for 10 fold cross validation
